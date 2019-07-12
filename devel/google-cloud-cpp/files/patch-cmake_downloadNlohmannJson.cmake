d931d4cc306d98cd2f658f662ffb1873776cc1c2
Anthony <amzo1337@gmail.com>
2019-07-12 21:21:43 +0100

    Adding google-cloud-cpp port

---
+++ devel/google-cloud-cpp/files/patch-cmake_downloadNlohmannJson.cmake
@@ -0,0 +1,14 @@
+--- cmake/DownloadNlohmannJson.cmake.orig
++++ cmake/DownloadNlohmannJson.cmake
+@@ -37,6 +37,10 @@ function (_download_json_hpp)
+             execute_process(COMMAND "${CMAKE_COMMAND}" -E sleep
+                                     "${sleep_seconds}")
+         endif ()
++        if (EXISTS "${DEST}/json.hpp")
++            set(download_error_code 0)
++            break()
++        endif ()
+         file(DOWNLOAD "${JSON_URL}" "${DEST}/json.hpp" STATUS download_status)
+         list(GET download_status 0 download_error_code)
+         if (download_error_code EQUAL 0)
+
