9ebd844f793a294590606ba77d40d7651681f653
Anthony <amzo1337@gmail.com>
2019-07-13 19:38:06 +0100

    fix patch path location

--- devel/google-cloud-cpp/files/patch-cmake_GoogleCloudCppCommon.cmake
+++ devel/google-cloud-cpp/files/patch-cmake_GoogleCloudCppCommon.cmake
@@ -1,5 +1,5 @@
---- /usr/home/Amzo/py-tensorflow/devel/google-cloud-cpp/work/google-cloud-cpp-cc1af99/cmake/GoogleCloudCppCommon.cmake.orig	2019-07-12 21:04:09.526957000 +0100
-+++ /usr/home/Amzo/py-tensorflow/devel/google-cloud-cpp/work/google-cloud-cpp-cc1af99/cmake/GoogleCloudCppCommon.cmake	2019-07-12 21:05:37.289511000 +0100
+--- cmake/GoogleCloudCppCommon.cmake.orig	2019-07-12 21:04:09.526957000 +0100
++++ cmake/GoogleCloudCppCommon.cmake	2019-07-12 21:05:37.289511000 +0100
 @@ -24,7 +24,6 @@
  if (NOT MSVC)
      check_cxx_compiler_flag(-Wall GOOGLE_CLOUD_CPP_COMPILER_SUPPORTS_WALL)
